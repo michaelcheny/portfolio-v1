d03457e76ab7c9428d470cd7ccc4ffbb15eee653
Michael Chen <michaelchengaming@gmail.com>
2020-05-30 01:28:16 -0700

    add a about container page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import AboutContainer from "./containers/AboutContainer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <h1 className="text-6xl">hi</h1>
+      <h1 className="text-6xl">PLACEHOLDER FOR IMAGE??</h1>
+      <AboutContainer />
     </div>
   );
 };
---
+++ src/containers/AboutContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutContainer = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-dracula2" id="about">
+        About
+      </h1>
+      <p className="text-dracula2">
+        God? God's turning people into insect monsters Beth. I'm the one beating them to death. Thank me.
+        Nobody's killing me until after I catch my wife with another man. Listen to your sister Morty; to
+        live is to risk it all; otherwise you're just an inert chunk of randomly assembled molecules
+        drifting wherever the universe blows you. Oh, I'm sorry, Jerry, I didn't see you there, how much
+        of that did you hear? Do you know how many characters there are in the Simpsons Morty? There's
+        like a-a billion characters, M-Morty. There was an episode where Former President BUSH was their
+        neighbor!
+      </p>
+    </div>
+  );
+};
+
+export default AboutContainer;
