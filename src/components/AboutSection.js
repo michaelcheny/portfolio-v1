96357ec341210c2a1f2ceed62d7736511c74f376
Michael Chen <michaelchengaming@gmail.com>
2020-05-31 13:58:53 -0700

    change the div to section for the setions

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const AboutContainer = () => {
   return (
-    <div className="h-screen max-w-screen-lg mx-auto">
-      <h1 className="text-5xl text-gray-500" id="about">
-        About
-      </h1>
-      <p className="text-gray-400">
-        God? God's turning people into insect monsters Beth. I'm the one beating them to death. Thank me.
-        Nobody's killing me until after I catch my wife with another man. Listen to your sister Morty; to
-        live is to risk it all; otherwise you're just an inert chunk of randomly assembled molecules
-        drifting wherever the universe blows you. Oh, I'm sorry, Jerry, I didn't see you there, how much
-        of that did you hear? Do you know how many characters there are in the Simpsons Morty? There's
-        like a-a billion characters, M-Morty. There was an episode where Former President BUSH was their
-        neighbor!
-      </p>
-    </div>
+    <section className="h-screen max-w-screen-lg mx-auto">
+      <div>
+        <h1 className="text-5xl text-gray-500" id="about">
+          About
+        </h1>
+        <p className="text-gray-400">
+          God? God's turning people into insect monsters Beth. I'm the one beating them to death. Thank
+          me. Nobody's killing me until after I catch my wife with another man. Listen to your sister
+          Morty; to live is to risk it all; otherwise you're just an inert chunk of randomly assembled
+          molecules drifting wherever the universe blows you. Oh, I'm sorry, Jerry, I didn't see you
+          there, how much of that did you hear? Do you know how many characters there are in the Simpsons
+          Morty? There's like a-a billion characters, M-Morty. There was an episode where Former
+          President BUSH was their neighbor!
+        </p>
+      </div>
+    </section>
   );
 };
 
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -2,33 +2,35 @@ import React from "react";
 
 const ProjectsSection = () => {
   return (
-    <div className="h-screen max-w-screen-lg mx-auto">
-      <h1 className="text-5xl text-gray-500" id="projects">
-        Projects
-      </h1>
-      <p className="text-gray-400">
-        Well, to be honest, I'm kind of grossed out with the sexual nature of how everything unfolded. I
-        didn't know how sexual dragons were. I kind of just wanted to do some D&D stuff, y'know? I've
-        replaced them both as the de facto patriarch of your family and your universe. Your mom wouldn't
-        have accepted me if I came home without you and your sister, so now you know the real reason I
-        rescued you. I just took over the family, Morty! And if you tell your mom or sister I said any of
-        this, I'll deny it, and they'll take my side because I'm a hero, Morty! And now you're gonna have
-        to go and do whatever I say, Morty! Forever! An-and I-I-I'll go out and I'll find some more of
-        that Mulan szechuan teriyaki dipping sauce, Morty! Th-b-because that's what this is all about
-        Morty! Th-that's my one-armed man! I'm not driven by avenging my dead family, Morty, that was
-        fake! I-I-I'm driven by finding that mcnuggut sauce! I want that Mulan mcnuggut sauce, Morty!
-        That's my series arc, Morty! If it takes nine seasons, I want my mcnuggut dipping sauce szechuan
-        sauce, Morty! That's what's gon-it's gonna take us all the way to the end, Morty! Season nine
-        more seasons, Morty! Nine more seasons until I get that dipping szechuan sauce! For ninety-seven
-        more years, Morty! I want that mcnuggut sauce Morty! Whoa, spoilers! I'm a whole season behind.
-        You look it up, you don't- you don't even know what it means. Those guys are inside you building
-        a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck
-        those people, fuck this whole thing Ethan. Oh, that's Million Ants. I can't see the ants from
-        over here, I just assumed that was um.. Turd Man, Wiggly Turd Man. The trick to cereal is keeping
-        70% of it above the milk. "And"? What more do you want tacked on to this? I turned myself into a
-        pickle, and 9/11 was an inside job?"
-      </p>
-    </div>
+    <section className="h-screen max-w-screen-lg mx-auto">
+      <div>
+        <h1 className="text-5xl text-gray-500" id="projects">
+          Projects
+        </h1>
+        <p className="text-gray-400">
+          Well, to be honest, I'm kind of grossed out with the sexual nature of how everything unfolded.
+          I didn't know how sexual dragons were. I kind of just wanted to do some D&D stuff, y'know? I've
+          replaced them both as the de facto patriarch of your family and your universe. Your mom
+          wouldn't have accepted me if I came home without you and your sister, so now you know the real
+          reason I rescued you. I just took over the family, Morty! And if you tell your mom or sister I
+          said any of this, I'll deny it, and they'll take my side because I'm a hero, Morty! And now
+          you're gonna have to go and do whatever I say, Morty! Forever! An-and I-I-I'll go out and I'll
+          find some more of that Mulan szechuan teriyaki dipping sauce, Morty! Th-b-because that's what
+          this is all about Morty! Th-that's my one-armed man! I'm not driven by avenging my dead family,
+          Morty, that was fake! I-I-I'm driven by finding that mcnuggut sauce! I want that Mulan mcnuggut
+          sauce, Morty! That's my series arc, Morty! If it takes nine seasons, I want my mcnuggut dipping
+          sauce szechuan sauce, Morty! That's what's gon-it's gonna take us all the way to the end,
+          Morty! Season nine more seasons, Morty! Nine more seasons until I get that dipping szechuan
+          sauce! For ninety-seven more years, Morty! I want that mcnuggut sauce Morty! Whoa, spoilers!
+          I'm a whole season behind. You look it up, you don't- you don't even know what it means. Those
+          guys are inside you building a piece of shit Ethan! They're inside you building a monument to
+          compromise! Fuck them, fuck those people, fuck this whole thing Ethan. Oh, that's Million Ants.
+          I can't see the ants from over here, I just assumed that was um.. Turd Man, Wiggly Turd Man.
+          The trick to cereal is keeping 70% of it above the milk. "And"? What more do you want tacked on
+          to this? I turned myself into a pickle, and 9/11 was an inside job?"
+        </p>
+      </div>
+    </section>
   );
 };
 
