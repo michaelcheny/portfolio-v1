caf03a29af47013e88b3f165e423bdeb13f25953
Michael Chen <michaelchengaming@gmail.com>
2020-06-01 17:12:07 -0700

    add imapge placeholder to about section, fix margins and paddings and flexes and stuff

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,20 +2,25 @@ import React from "react";
 
 const AboutContainer = () => {
   return (
-    <section className="h-screen max-w-screen-lg mx-auto">
-      <div>
-        <h1 className="text-5xl text-gray-500" id="about">
-          About
-        </h1>
-        <p className="text-gray-400">
-          God? God's turning people into insect monsters Beth. I'm the one beating them to death. Thank
-          me. Nobody's killing me until after I catch my wife with another man. Listen to your sister
-          Morty; to live is to risk it all; otherwise you're just an inert chunk of randomly assembled
-          molecules drifting wherever the universe blows you. Oh, I'm sorry, Jerry, I didn't see you
-          there, how much of that did you hear? Do you know how many characters there are in the Simpsons
-          Morty? There's like a-a billion characters, M-Morty. There was an episode where Former
-          President BUSH was their neighbor!
-        </p>
+    <section className="h-screen max-w-screen-lg mx-auto flex" id="about">
+      <div className=" m-auto pb-20">
+        <h1 className="text-5xl text-gray-500">About</h1>
+        <div className="flex mb-4">
+          <img
+            className="border border-red-500 h-64 w-64"
+            src="https://cdn1.iconfinder.com/data/icons/image-manipulations/100/13-512.png"
+            alt="placeholder"
+          />
+          <p className="text-gray-400 pt-12 pl-3">
+            God? God's turning people into insect monsters Beth. I'm the one beating them to death. Thank
+            me. Nobody's killing me until after I catch my wife with another man. Listen to your sister
+            Morty; to live is to risk it all; otherwise you're just an inert chunk of randomly assembled
+            molecules drifting wherever the universe blows you. Oh, I'm sorry, Jerry, I didn't see you
+            there, how much of that did you hear? Do you know how many characters there are in the
+            Simpsons Morty? There's like a-a billion characters, M-Morty. There was an episode where
+            Former President BUSH was their neighbor!
+          </p>
+        </div>
       </div>
     </section>
   );
