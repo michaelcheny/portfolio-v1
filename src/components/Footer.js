9b0611e9d4aa3e8fef0c51e53bbb5a9748842d4a
Michael Chen <michaelchengaming@gmail.com>
2020-06-02 01:12:33 -0700

    add name to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <section id="footer">
-      <div className="block bg-nord-0 p-6">
+      <div className="block bg-dracula1 p-6">
         <ul className="list-none mx-auto text-center">
-          <li className="inline-block mx-1 border border-red-500">
+          <li className="inline-block mx-1 ">
             <FontAwesomeIcon icon="envelope-square" size="3x" color="white" />
           </li>
-          <li className="inline-block mx-1 border border-red-500">
+          <li className="inline-block mx-1">
             <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" color="white" />
           </li>
-          <li className="inline-block mx-1 border border-red-500">
+          <li className="inline-block mx-1 ">
             <FontAwesomeIcon icon={["fab", "github-square"]} size="3x" color="white" />
           </li>
-          <li className="inline-block mx-1 border border-red-500">
+          <li className="inline-block mx-1 ">
             <FontAwesomeIcon icon={["fab", "medium"]} size="3x" color="white" />
           </li>
         </ul>
+        <p className="text-center text-dracula4 py-2">© Michael Chen 2020</p>
       </div>
     </section>
   );
