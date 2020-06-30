b36c99f8dc9fa02821414258923b4dbe79db0c3e
Michael Chen <michaelchengaming@gmail.com>
2020-06-30 15:33:08 -0700

    remove unused assets, add github and youtube svg and change styling on projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,10 +1,12 @@
 import React from "react";
 import * as data from "../assets/projects/projects.json";
+import githubIcon from "../assets/images/github.svg";
+import youtubeIcon from "../assets/images/youtube.svg";
 
 const ProjectsSection = () => {
   return (
     <section className="flex justify-center items-center" id="projects">
-      <div className="flex flex-col justify-center items-center w-5/6 border border-dracula-5 my-16">
+      <div className="flex flex-col justify-center items-center w-5/6 my-32">
         <h3 className="text-2xl">
           Projects
           <hr className=" w-full h-0 pb-4 border-background-ternary border-t" />
@@ -25,52 +27,28 @@ export default ProjectsSection;
 
 const Project = ({ project }) => {
   return (
-    // <div className="bg-dracula-2 flex flex-col border border-background-ternary w-full h-120 my-8">
-    //   {project.name}
-    // </div>
-    // <div className="flex md:flex-row flex-wrap  flex-grow rounded overflow-hidden bg-background-primary shadow-2xl my-10 h-auto border-red-600 border break-words">
-    // <div>
-    //   <img
-    //     className=" h-auto object-scale-down border border-dracula-5"
-    //     src={project.image}
-    //     alt={`${project.name}`}
-    //   />
-    //   <div className="flex flex-col overflow-hidden">
-    //     <div className="px-6 py-4 break-words flex-wrap overflow-hidden">
-    //       <div className="font-bold text-xl mb-2 text-copy-primary">{project.name}</div>
-    //       <p className="text-copy-primary text-base border border-dracula-5 ">{project.description}</p>
-    //     </div>
-    //     <div className="px-6 py-4">
-    //       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-    //         #photography
-    //       </span>
-    //       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-    //         #travel
-    //       </span>
-    //       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-    //         #winter
-    //       </span>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="max-w-md rounded-sm overflow-hidden shadow-lg mb-4 self-center">
-      <img className="w-full" src={project.image} alt="Sunset in the mountains" />
+    <div className="max-w-sm xl:max-w-md rounded-sm m-8 overflow-hidden shadow-md mb-4 self-center text-copy-secondary">
+      <img className="w-full" src={project.image} alt={project.title} />
       <div className="px-6 py-4">
-        <div className="font-bold text-copy-secondary text-xl mb-2">{project.name}</div>
-        <p className="text-base">
-          {project.description} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
+        <div className=" mb-2">
+          <span className="font-bold text-xl ">{project.name}</span>
+          <div className="inline-block float-right">
+            <a className="inline-block" href={project.githubLink}>
+              <img className="h-8 mr-2" src={githubIcon} alt="github" title="GitHub" />
+            </a>
+            <a className="inline-block" href={project.demoLink}>
+              <img className="h-8" src={youtubeIcon} alt="youtube" title="Demo" />
+            </a>
+          </div>
+        </div>
+        <p className="text-base">{project.description}</p>
       </div>
-      <div className="px-6 py-4">
+      <div className="px-6 pb-3 text-xs">
         {project.techStack.map((stack) => (
-          <span className="inline-block bg-background-primary rounded-sm px-3 py-1 text-sm font-semibold text-copy-primary mr-2">
+          <span className="inline-block bg-background-secondary rounded-sm px-3 py-1 text-sm font-normal text-copy-primary mr-2">
             {stack}
           </span>
         ))}
-        <a className="block" href={project.githubLink}>
-          GitHub
-        </a>
       </div>
     </div>
   );
